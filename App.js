4b55fd066d363e140e5f07d6e7b7ba69bf603dbd
Guillermo Machado <87659688+guillermomachado@users.noreply.github.com>
2024-02-26 16:26:10 -0300

    Rebase fixes

--- App.js
+++ App.js
@@ -21,66 +21,69 @@ import Ramarasa from './src/screens/Ramarasa';
 import {COLORS} from './src/constants/styles';
 import DeleteAccountGame from './src/features/deleteAccountGame';
 import ReactToPost from './src/features/ReactToPost';
+import {Provider} from 'react-native-paper';
 
 const Stack = createStackNavigator();
 
 const App = () => (
-  <NavigationContainer>
-    <Stack.Navigator>
-      <Stack.Screen
-        name={DASHBOARD}
-        component={Dashboard}
-        options={{headerTitle: 'Bad UX Animation Challenge'}}
-      />
-      <Stack.Screen
-        name={MANU_GENIA}
-        component={ManuGenia}
-        options={{headerTitle: 'How Old Are You?'}}
-      />
-      <Stack.Screen
-        name={WHATS_YOUR_NAME}
-        component={WhatsYourName}
-        options={{headerTitle: "What's your name?"}}
-      />
-      <Stack.Screen
-        name={DELETE_ACCOUNT}
-        component={DeleteAccount}
-        options={{
-          headerTitle: 'Delete account',
-          headerStyle: {
-            backgroundColor: COLORS.black,
-          },
-          headerTintColor: COLORS.white,
-        }}
-      />
-      <Stack.Screen
-        name={DELETE_ACCOUNT_GAME}
-        component={DeleteAccountGame}
-        options={{headerTitle: "Let's play"}}
-      />
-      <Stack.Screen
-        name={DELETE_ACCOUNT_CALA}
-        component={DeleteAccountCala}
-        options={{
-          headerTitle: 'Delete account',
-          headerStyle: {
-            backgroundColor: COLORS.black,
-          },
-          headerTintColor: COLORS.white,
-        }}
-      />
-      <Stack.Screen
-        name={RAMA_RASA}
-        component={Ramarasa}
-        options={{headerTitle: 'Ramarasa Corp. 👀'}}
-      />
-      <Stack.Screen
-        name={REACT_TO_POST}
-        component={ReactToPost}
-        options={{headerTitle: 'React'}}
-      />
-    </Stack.Navigator>
-  </NavigationContainer>
+  <Provider>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name={DASHBOARD}
+          component={Dashboard}
+          options={{headerTitle: 'Bad UX Animation Challenge'}}
+        />
+        <Stack.Screen
+          name={MANU_GENIA}
+          component={ManuGenia}
+          options={{headerTitle: 'How Old Are You?'}}
+        />
+        <Stack.Screen
+          name={WHATS_YOUR_NAME}
+          component={WhatsYourName}
+          options={{headerTitle: "What's your name?"}}
+        />
+        <Stack.Screen
+          name={DELETE_ACCOUNT}
+          component={DeleteAccount}
+          options={{
+            headerTitle: 'Delete account',
+            headerStyle: {
+              backgroundColor: COLORS.black,
+            },
+            headerTintColor: COLORS.white,
+          }}
+        />
+        <Stack.Screen
+          name={DELETE_ACCOUNT_GAME}
+          component={DeleteAccountGame}
+          options={{headerTitle: "Let's play"}}
+        />
+        <Stack.Screen
+          name={DELETE_ACCOUNT_CALA}
+          component={DeleteAccountCala}
+          options={{
+            headerTitle: 'Delete account',
+            headerStyle: {
+              backgroundColor: COLORS.black,
+            },
+            headerTintColor: COLORS.white,
+          }}
+        />
+        <Stack.Screen
+          name={RAMA_RASA}
+          component={Ramarasa}
+          options={{headerTitle: 'Ramarasa Corp. 👀'}}
+        />
+        <Stack.Screen
+          name={REACT_TO_POST}
+          component={ReactToPost}
+          options={{headerTitle: 'React'}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  </Provider>
 );
 
 export default App;
